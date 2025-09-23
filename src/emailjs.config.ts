6d3dd1e6c97fcf98705626bb2a43183f417f9ac4
Carlos Valente <carlos.valente@vwds.pt>
2025-09-24 00:53:53 +0100

    simulador saude Refactor code structure for improved readability and maintainability

--- src/emailjs.config.ts
+++ src/emailjs.config.ts
@@ -2,3 +2,8 @@
 export const EMAILJS_SERVICE_ID = "service_g957e2d";
 export const EMAILJS_TEMPLATE_ID = "template_e1b2bg9";
 export const EMAILJS_USER_ID = "BSaau8RnIf4S9eYV6"; 
+
+// IDs específicos para o formulário de Saúde
+export const EMAILJS_SERVICE_ID_SAUDE = "service_gk9trk7";
+export const EMAILJS_TEMPLATE_ID_SAUDE = "template_cinva8e";
+export const EMAILJS_USER_ID_SAUDE = "rhKWX9EC7oQt2nwPK";
